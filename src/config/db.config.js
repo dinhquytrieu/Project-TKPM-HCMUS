96d32b0b24adc49d2645e32a47718e3e48c45adc
phuchaitt3 <phuchaitt3@gmail.com>
2024-05-12 17:19:40 +0700

    Comments for db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,3 +1,10 @@
+// Configuration for establishing a connection to a MongoDB database using Mongoose in a Node.js application. 
+// It is structured as a module that can be reused to connect to the database from various parts of the application.
+
+// While this module does not strictly implement the Singleton pattern (as it does not restrict the instantiation of itself to one instance), 
+// it effectively enforces a single MongoDB connection throughout the application. The use of the module pattern here ensures that 
+// every part of the application that requires a database connection will reuse the same connection initialized by this connect function. 
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
